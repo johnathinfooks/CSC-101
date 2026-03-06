1be104daec006ed6520748d4fbe4e9c8b9382404
ShortVincentMan <vinvyle06@gmail.com>
2026-03-05 19:27:34 -0800

    ADD: Flag check

--- src/user.py
+++ src/user.py
@@ -21,9 +21,20 @@ class User:
 
         return s
     
+    def check_flag(self):
+        mal_sum = 0
+        threshold = 50 #set threshold here
+        for i in self.message_history:
+            score = i.check_malicious()
+            mal_sum += score if score else 0
+        self.malicious_score = mal_sum
+        if mal_sum >= threshold: 
+            self.malicious_flag = True
+
+
 def display_flagged_users(users: list[User], threshold: int) -> str:
     flagged_list = []
     for i in users:
         if i.malicious_flag == True:
-            flagged_list.append[i]
+            flagged_list.append(i)
     print(f"Flagged:{flagged_list}")
